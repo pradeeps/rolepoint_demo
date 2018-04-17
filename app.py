7f9a5eaeb785dec88729b951ce05bf764c771582
Pradeep Susarla <pradeep@pradeeps.org>
2018-04-17 23:37:32 +0100

    Added SQLALCHEMY configuration and database file Added index.html file

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+import os
+
 from flask import Flask
+from flask import render_template
+
+basedir = os.path.dirname(os.path.abspath(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(basedir, 'contacts.db'))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
